c95712eb76ad0bc8806a974dc4890632fb359eb5
Denise Khuu <65967082+denisekhuu@users.noreply.github.com>
2024-06-13 13:35:39 +0200

    [Working] React Base

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js",
+    library: 'etabase',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      'eta-base': path.resolve(__dirname, 'src'), // Adjust based on your source folder
+    },
+  },
+};
